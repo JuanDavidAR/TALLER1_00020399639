5dedce8bf3eebb3cfe221162e76d4503863937e8
JuanDavidAR <aldana.juand@javeriana.edu.co>
2022-02-14 17:48:03 -0500

    Create javascript.js

---
+++ MiPagina/Contacto/javascript.js
@@ -0,0 +1,6 @@
+function captura(){
+    var nombre=document.getElementById("nombres").value;
+    var tel=document.getElementById("telefono").value;
+    var corr=document.getElementById("correo").value;
+    alert("Los datos ingresados fueron:\n"+ nombre +"\n" +tel+"\n"+ corr+"\nNos pondremos en contacto con usted pronto!");
+}
